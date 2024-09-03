953a972cabd361a4412bccd6548776c3003b72ec
Manu_Love <107737902+mloveral@users.noreply.github.com>
2024-09-03 18:36:01 -0500

    Métodos Vuelo implementados

--- src/uniandes/dpoo/aerolinea/modelo/Vuelo.java
+++ src/uniandes/dpoo/aerolinea/modelo/Vuelo.java
@@ -1,11 +1,13 @@
 package uniandes.dpoo.aerolinea.modelo;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 import uniandes.dpoo.aerolinea.exceptions.VueloSobrevendidoException;
 import uniandes.dpoo.aerolinea.modelo.cliente.Cliente;
 import uniandes.dpoo.aerolinea.modelo.tarifas.CalculadoraTarifas;
+import uniandes.dpoo.aerolinea.tiquetes.GeneradorTiquetes;
 import uniandes.dpoo.aerolinea.tiquetes.Tiquete;
 
 public class Vuelo {
@@ -20,6 +22,7 @@ public class Vuelo {
 		this.ruta = ruta;
 		this.fecha = fecha;
 		this.avion = avion;
+		this.tiquetes = new HashMap<String, Tiquete>();
 	}
 
 	public Ruta getRuta() {
@@ -40,11 +43,47 @@ public class Vuelo {
 		return tiquetesVuelo;
 	}
 	
+	/**
+	 * Vende una determinada cantidad de tiquetes para el vuelo y los deja registrados en el mapa de tiquetes
+	 * @param cliente El cliente al cual se le venden los tiquetes
+	 * @param calculadora La calculadora de tarifas que debe usarse para saber el precio por tiquete
+	 * @param cantidad La cantidad de tiquetes que se quieren comprar
+	 * @return El valor total de los tiquetes vendidos
+	 * @throws VueloSobrevendidoException Se lanza esta excepción si no hay suficiente espacio en el vuelo para todos los pasajeros
+	 */
 	public int venderTiquetes(Cliente cliente, CalculadoraTarifas calculadora, int cantidad) throws VueloSobrevendidoException {
-		return 0;
+		if (this.avion.getCapacidad() > this.tiquetes.size() + cantidad) {
+			throw new VueloSobrevendidoException(this);
+		}
+		int valorTotal = 0;
+		int tarifaXTiquete = calculadora.calcularTarifa(this, cliente);
+		Tiquete tiquete;
+		for (int i = 0; i < cantidad; i++)
+		{
+			tiquete = GeneradorTiquetes.generarTiquete(this, cliente, tarifaXTiquete);
+			this.tiquetes.put(tiquete.getCodigo(), tiquete);
+			cliente.agregarTiquete​(tiquete);
+			valorTotal += tarifaXTiquete;
+		}
+		return valorTotal;
 	}
 	
+	@Override
 	public boolean equals(Object obj) {
-		return false;
+		Vuelo vueloCompare = (Vuelo) obj;
+		boolean igual = false;
+		if (this.avion.getNombre().equals(vueloCompare.avion.getNombre()))
+		{
+			if (this.fecha.equals(vueloCompare.fecha))
+			{
+				Ruta r = this.getRuta();
+				Ruta rC = vueloCompare.getRuta();
+				if (r.getOrigen().equals(rC.getOrigen()) && r.getDestino().equals(rC.getDestino())) 
+				{
+					igual = true;
+				}
+			}
+		}
+		return igual;
 	}
 }
